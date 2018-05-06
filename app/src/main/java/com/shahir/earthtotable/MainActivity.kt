3a3967910c4d97cd85a19e86efc04b58fe1e3e23
Monique Robinson <msrobin@khudra>
2018-05-06 17:23:58 -0400

    updated main screen to have a button

--- app/src/main/java/com/shahir/earthtotable/MainActivity.kt
+++ app/src/main/java/com/shahir/earthtotable/MainActivity.kt
@@ -3,6 +3,7 @@ package com.shahir.earthtotable
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+//this the main activity file for earthtotable
 
 
 class MainActivity : AppCompatActivity() {
