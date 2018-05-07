cb32510a3241b0663b8eeffe4388ba97353d6cc5
Monique <msrobinconsulting@gmail.com>
2018-05-06 21:09:38 -0400

    second activity

--- app/src/main/java/com/shahir/earthtotable/MainActivity.kt
+++ app/src/main/java/com/shahir/earthtotable/MainActivity.kt
@@ -1,17 +1,44 @@
 package com.shahir.earthtotable
 
+import android.content.Intent
 import android.os.Bundle
+import android.support.v4.content.ContextCompat.startActivity
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 //this the main activity file for earthtotable
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
 
+    /** onCreate is called when the app is first launched.
+     * @param savedInstanceState is the saved state of the application.
+     *
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        veggies_basket
+        button.setOnClickListener(
+               val intent = Intent (this,findmore::class.java)
+               startActivity(intent)
+            )
     }
+
+    override fun onClick(v: View?) {
+        when (v.id) {
+            R.id.button -> findmore()
+            else -> println("error in file")
+        }
+    }
+
+    // create function findmore to be called when user clicks on find more button
+    private fun findmore() {
+
+        val nextScreenIntent = Intent(packageContext:this, findmore::class.java).apply {
+
+        }
+        startActivity(nextScreenIntent)
+    }
+
 }
